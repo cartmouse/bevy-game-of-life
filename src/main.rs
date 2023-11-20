f8e18a6c530ceb056213092db5f9738bcb642dce
Tom Cartwright <cartmouse@outlook.com>
2023-11-20 11:47:40 +0000

    Render grid

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,36 @@ fn main() {
     App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn_batch(create_grid());
+}
+
+struct Grid {
+    rows: i32,
+    cols: i32,
+}
+
+fn create_grid() -> Vec<SpriteBundle> {
+    let mut bundle = vec![];
+    let grid = Grid { rows: 20, cols: 20 };
+    for row in 0..grid.rows {
+        for col in 0..grid.cols {
+            bundle.push(SpriteBundle {
+                sprite: Sprite {
+                    custom_size: Some(Vec2 { x: 10.0, y: 10.0 }),
+                    ..Default::default()
+                },
+                transform: Transform::from_translation(Vec3::new(
+                    row as f32 * (10.0 + 2.0),
+                    col as f32 * (10.0 + 2.0),
+                    0.0,
+                )),
+                ..Default::default()
+            })
+        }
+    }
+    return bundle;
 }
 
 #[derive(Resource)]
@@ -32,7 +58,7 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
+            .add_systems(Startup, setup)
             .add_systems(Update, greet_people);
     }
 }
