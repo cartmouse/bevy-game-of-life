0fcf8e817da558f72603a84320ca07bb4cfe0067
Tom Cartwright <cartmouse@outlook.com>
2023-11-21 09:33:36 +0000

    Add hover states for grid

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use bevy::prelude::*;
+use bevy::render::settings::{Backends, WgpuSettings};
+use bevy::render::RenderPlugin;
+use bevy::window::PrimaryWindow;
+
+const CELL_SIZE: f32 = 20.0;
+const HALF_SIZE: f32 = CELL_SIZE / 2.0;
+const HOVER_SIZE: f32 = CELL_SIZE + 1.0;
+const GAP_SIZE: f32 = 2.0;
 
 fn main() {
-    App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: WgpuSettings {
+                    backends: Some(Backends::VULKAN),
+                    ..default()
+                }
+                .into(),
+            }),
+            AutoKitchenPlugin,
+        ))
+        .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, q_windows: Query<&Window, With<PrimaryWindow>>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn_batch(create_grid());
+    commands.spawn_batch(create_grid(q_windows.single()));
 }
 
 struct Grid {
@@ -14,51 +33,80 @@ struct Grid {
     cols: i32,
 }
 
-fn create_grid() -> Vec<SpriteBundle> {
+#[derive(Component)]
+struct Cell {
+    hovered: bool,
+    coord: Vec2,
+}
+
+fn create_grid(window: &Window) -> Vec<(Cell, SpriteBundle)> {
     let mut bundle = vec![];
     let grid = Grid { rows: 20, cols: 20 };
     for row in 0..grid.rows {
         for col in 0..grid.cols {
-            bundle.push(SpriteBundle {
-                sprite: Sprite {
-                    custom_size: Some(Vec2 { x: 10.0, y: 10.0 }),
+            let coord = Vec2::new(
+                row as f32 * (CELL_SIZE + GAP_SIZE)
+                    - grid.rows as f32 * (CELL_SIZE + GAP_SIZE) * 0.5,
+                col as f32 * (CELL_SIZE + GAP_SIZE) - window.height() / 4.0,
+            );
+            let cell = Cell {
+                hovered: false,
+                coord,
+            };
+            bundle.push((
+                cell,
+                SpriteBundle {
+                    sprite: Sprite {
+                        custom_size: Some(Vec2 {
+                            x: CELL_SIZE,
+                            y: CELL_SIZE,
+                        }),
+                        ..Default::default()
+                    },
+                    transform: Transform::from_translation(Vec3::new(coord.x, coord.y, 0.0)),
                     ..Default::default()
                 },
-                transform: Transform::from_translation(Vec3::new(
-                    row as f32 * (10.0 + 2.0),
-                    col as f32 * (10.0 + 2.0),
-                    0.0,
-                )),
-                ..Default::default()
-            })
+            ));
         }
     }
     return bundle;
 }
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+fn cursor_position(
+    q_windows: Query<&Window, With<PrimaryWindow>>,
+    mut q_cells: Query<(&mut Cell, &mut Sprite)>,
+) {
+    let size = Vec2::new(q_windows.single().width(), q_windows.single().height());
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello, {}!", name.0);
+    if let Some(position) = q_windows.single().cursor_position() {
+        let new_pos = Vec2::new(
+            position.x - size.x / GAP_SIZE,
+            size.y / GAP_SIZE - position.y,
+        );
+
+        for (mut cell, mut sprite) in q_cells.iter_mut() {
+            if cell.coord.x <= new_pos.x + HALF_SIZE
+                && cell.coord.x >= new_pos.x - HALF_SIZE
+                && cell.coord.y <= new_pos.y + HALF_SIZE
+                && cell.coord.y >= new_pos.y - HALF_SIZE
+            {
+                cell.hovered = true;
+                sprite.custom_size = Some(Vec2::new(HOVER_SIZE, HOVER_SIZE));
+                sprite.color = Color::GREEN;
+            } else {
+                cell.hovered = false;
+                sprite.custom_size = Some(Vec2::new(CELL_SIZE, CELL_SIZE));
+                sprite.color = Color::WHITE;
+            }
         }
     }
 }
 
-#[derive(Component)]
-struct Person;
+pub struct AutoKitchenPlugin;
 
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
+impl Plugin for AutoKitchenPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, setup)
-            .add_systems(Update, greet_people);
+        app.add_systems(Update, cursor_position)
+            .add_systems(Startup, setup);
     }
 }
